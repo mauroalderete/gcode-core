d0beea2a9469bfebd44699fff922de994b5d3b17
Mauro Alderete <alderete.mauro@gmail.com>
2022-06-20 03:31:13 -0300

    sketch main

---
+++ cmd/skew/skew.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func exitGracefully(err error) {
+// 	fmt.Fprintf(os.Stderr, "error: %v\n", err)
+// 	os.Exit(1)
+// }
+
+// func check(e error) {
+// 	if e != nil {
+// 		exitGracefully(e)
+// 	}
+// }
+
+func main() {
+	fmt.Println("skew test")
+}
